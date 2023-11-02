3c0d7ab3778bcc15316a183c28f38e266822dd99
Серов Алексей <a.serov@m-developer.ru>
2023-11-02 20:46:03 +0300

    add mqtt, config

--- index.ts
+++ index.ts
@@ -27,40 +27,6 @@ async function main() {
             }
         }
     })
-    // await relay.setMultiple({
-    //     o1: true,
-    //     o2: true,
-    //     o3: true,
-    // });
-    // await new Promise((r)=>setTimeout(r, 1000));
-
-
-    // await relay.setMultiple({
-    //     o1: false,
-    //     o2: false,
-    //     o3: false,
-    // });
-
-    // await relay.off(1);
-    // await queue.dispose();
-
-    // const r1 = await client.connectRTUBuffered('/dev/ttyACM0', { baudRate: 9600, dataBits: 8, stopBits: 2 });
-    // const r2 = await client.setTimeout(500);
-    // const r3 = await client.setID(243);
-    // const result = await client.readHoldingRegisters(128, 1);
-    // console.log(result);
-    // const result2 = await client.readInputRegisters(0x00C8, 6);
-    // console.log(result2.buffer.toString());
-
-    // await client.writeCoil(3, 1);
-
-    // const coilStatus = await client.readCoils(0, 6);
-    // console.log(coilStatus);
-
-    // await new Promise((r)=>setTimeout(r, 1000));
-
-    // await client.writeCoil(3, 0);
-    // await client.close();
 }
 
 main();
---
+++ types/config.d.ts
@@ -0,0 +1,6 @@
+export interface Config {
+    mqtt: {
+        login: string;
+        password: string;
+    }
+}
