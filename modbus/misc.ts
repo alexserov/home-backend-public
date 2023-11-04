feac420a4e6f8b939e9461acddbd17a631902234
Серов Алексей <a.serov@m-developer.ru>
2023-11-04 22:06:26 +0300

    moving from ts to golang

--- modbus/misc.ts
+++
@@ -1,3 +0,0 @@
-import ModbusRTU from "modbus-serial";
-
-export type ModbusAction<T = void> = (client: ModbusRTU)=>Promise<T>;
--- types/config.d.ts
+++
@@ -1,6 +0,0 @@
-export interface Config {
-    mqtt: {
-        login: string;
-        password: string;
-    }
-}
