5746967f1a9da6d9a2028f33fc08fc9af4e9e293
Серов Алексей <a.serov@m-developer.ru>
2023-11-04 20:35:38 +0300

    wip state tracking

--- go/modbus/devices/manager/index.go
+++ go/modbus/devices/manager/index.go
@@ -1,25 +1,66 @@
 package manager
 
 import (
+	devicesRefreshable "serov/home-backend-public/modbus/devices/refreshable"
 	"sync"
+	"time"
 )
 
 var once sync.Once
 var instance *manager
 
 type manager struct {
-
+	items []devicesRefreshable.Refreshable
+	ticker *time.Ticker
+	disposeChannel chan struct{}
+	disposed bool
 }
 
 type Manager interface {
-
+	Register(item devicesRefreshable.Refreshable)
+	Dispose()
 }
 
 func Instance() Manager {
 	once.Do(func() {
 		instance = &manager{}
+		instance.initialize()
 	})
 
 	return instance
 }
 
+func (manager *manager)initialize() {
+	manager.ticker = time.NewTicker(200 * time.Millisecond)
+	manager.disposeChannel = make (chan struct{})
+	go func () {
+		for {
+			select {
+			case <-manager.ticker.C:
+				manager.processActions()
+			case <-manager.disposeChannel:
+				manager.ticker.Stop()
+				manager.disposed = true
+			}
+		}
+	}()
+}
+
+func (manager *manager)processActions() {
+	if manager.disposed {
+		return
+	}
+
+	for _, current := range manager.items {
+		(current).Refresh()
+	}
+}
+
+func (manager *manager)Dispose() {
+	manager.disposeChannel <- struct{}{}
+
+}
+
+func (manager *manager) Register(item devicesRefreshable.Refreshable) {
+	manager.items = append(manager.items, item)
+}
--- go/modbus/devices/relay/index.go
+++ go/modbus/devices/relay/index.go
@@ -1,8 +1,11 @@
 package relay
 
 import (
-	modbusQueue "serov/home-backend-public/modbus/queue"
+	"bytes"
+	"encoding/binary"
+	"serov/home-backend-public/modbus/devices/manager"
 	devicesRefreshable "serov/home-backend-public/modbus/devices/refreshable"
+	modbusQueue "serov/home-backend-public/modbus/queue"
 
 	"github.com/grid-x/modbus"
 )
@@ -26,14 +29,18 @@ func Create(slaveId byte) Relay {
 	return result
 } 
 
-func (relay *relay) invoke(action func(client modbus.Client) ([]byte, error)) ([]byte, error){
-	cResults := make (chan []byte)
+type generic[V any] struct {
+	relay *relay
+}
+
+func (generic generic[V]) invokeGeneric(action func(client modbus.Client) (V, error)) (V, error){
+	cResults := make (chan V)
 	defer close(cResults)
 
 	cErr := make(chan error)
 	defer close(cErr)
 
-	queue.Enqueue(relay.slaveId, func (client modbus.Client)  {
+	queue.Enqueue(generic.relay.slaveId, func (client modbus.Client)  {
 		r,e := action(client)
 		cResults <- r
 		cErr <- e
@@ -43,6 +50,9 @@ func (relay *relay) invoke(action func(client modbus.Client) ([]byte, error)) ([
 	err :=  <-cErr
 
 	return results, err
+}
+func (relay *relay) invoke(action func(client modbus.Client) ([] byte, error)) ([] byte, error){
+	return generic[[]byte]{relay}.invokeGeneric(action)
 }
 
 func (relay *relay) initialize() {
@@ -56,6 +66,8 @@ func (relay *relay) initialize() {
 	if val[1] != relay.slaveId {
 		panic("invalid slave id")
 	}
+
+	manager.Instance().Register(relay)
 }
 
 func (relay *relay) State() state {
@@ -63,5 +75,39 @@ func (relay *relay) State() state {
 }
 
 func (relay *relay) Refresh() {
-	
+	val, err:= generic[state]{relay}.invokeGeneric(func (cl modbus.Client) (state, error)  {
+		newState := relay.State()
+
+		outputs, coilsErr := cl.ReadCoils(0, 6)
+
+		if coilsErr == nil {
+			for i := 0; i<6; i++ {
+				newState.outputs[i] = outputs[0] & (1 << byte(i)) > 0
+			}
+		}
+
+		inputs, inputsErr := cl.ReadDiscreteInputs(0, 8)
+		if inputsErr == nil {
+			for i := 0; i<6; i++ {
+				newState.inputs[i] = inputs[0] & (1 << byte(i)) > 0
+			}
+			newState.inputs[0] = inputs[0] & (1 << 7) > 0
+		}
+
+		counters, countersErr := cl.ReadInputRegisters(32, 8)
+		uCounters := make([]uint16, 8)
+		binary.Read(bytes.NewReader(counters), binary.BigEndian, uCounters)
+		if countersErr == nil {
+			for i, val := range counters[:5] {
+				newState.inputs[i+1] = val>0
+			}
+			newState.inputs[0] = counters[7] > 0
+		}
+
+		return newState, nil
+	})
+
+	if err == nil {
+		relay.state = val
+	}
 }
