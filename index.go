0226b78001c3be1785875dae79aa43ae2be2efac
aserov <a.serov@m-developer.ru>
2024-05-04 22:17:41 +0300

    fix switch index

--- index.go
+++ index.go
@@ -75,7 +75,7 @@ func fetchAndProcessCommands() {
 		}
 
 		zap.L().Debug("new value", zap.Any("relay id", relayItem.Id()), zap.Any("switch", device.SwitchId), zap.Any("value", newValue))
-		relayItem.Set(byte(device.SwitchId+1), newValue)
+		relayItem.Set(byte(device.SwitchId), newValue)
 	}
 }
 
